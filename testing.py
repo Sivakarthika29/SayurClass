b8ed04dad281ea42ffff9b4cdee16898dac7cefa
Sivakarthika29 <sivakarthika.sekar29@gmail.com>
2023-08-30 22:57:27 +0530

    newHW

--- testing.py
+++ testing.py
@@ -1,13 +1,1 @@
-# li = ["apple","mango","butter"]
-# for i in li:
-#     print(i)
-
-li = [2,3,4]
-# for i in range(len(li)):
-#     print(li[i])
-for i in li:
-    print(f"1 * {i} = {1*i}")
-    print(f"2 * {i} = {2*i}")
-    print(f"3 * {i} = {3*i}")
-    print(f"4 * {i} = {4*i}")
-    print(f"5 * {i} = {5*i}")
+print("summmsmaas")
