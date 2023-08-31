9332ef4f3038ee054a2fb97ea2046760f3243eb3
Sivakarthika29 <sivakarthika.sekar29@gmail.com>
2023-08-31 23:34:30 +0530

    changes

--- Day 1/Variables.py
+++ Day 1/Variables.py
@@ -1,3 +1,10 @@
+'''Suppose,  there are three students, first student brought Rs.1000 and second student brought 800 and another structure brough 1200. They went spent 2000 at weekend outing. They have 1000 amount is pending and we want to distribute back to the students. 
+1.How do you want to distribute them ? 
+write short progam to express your idea through python program
+How to apply same logic, Suppose 10 students and each student brought 500.
+
+through this homework, we are exploring  basic variables and operations and input and output operations.'''
+
 student_1=int(input("Enter the Student_1 amount:"))
 student_2=int(input("Enter the Student_2 amount:"))
 student_3=int(input("Enter the Student_3 amount:"))
