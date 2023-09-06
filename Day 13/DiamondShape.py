91a89b03147f67681bbe78f23656aa8f363762a4
Sivakarthika29 <sivakarthika.sekar29@gmail.com>
2023-09-06 18:55:15 +0530

    commit

---
+++ Day 13/DiamondShape.py
@@ -0,0 +1,39 @@
+######## Problem 1 ###############
+#Write a program that prints out a diamond shape using $.
+#After printing each line, wait for user input. If the user presses space, continue
+#If the users presses any other key, stop printing. Maximum 10 lines
+
+
+line = int(input("Enter the number of lines: "))
+for i in range(line):
+    if input("Enter space to print $") == " ":
+        print(" " * (line-i) + "$ " * (i+1))
+    else:
+        break
+for j in range(line - 1):
+    if input("Enter space to print $") == " ":
+        print(" " * (j+2) + "$ " * (line -1-j) )
+    else:
+        break
+
+# OUTPUT:
+
+# Enter the number of lines: 5
+# Enter space to print $ 
+#      $
+# Enter space to print $ 
+#     $ $
+# Enter space to print $ 
+#    $ $ $
+# Enter space to print $ 
+#   $ $ $ $ 
+# Enter space to print $ 
+#  $ $ $ $ $ 
+# Enter space to print $ 
+#   $ $ $ $ 
+# Enter space to print $ 
+#    $ $ $ 
+# Enter space to print $ 
+#     $ $ 
+# Enter space to print $ 
+#      $ 
