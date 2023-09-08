3f22cb7e5a79b491e0176a32ef5ddaacfb88bb15
Sivakarthika29 <sivakarthika.sekar29@gmail.com>
2023-09-08 07:04:59 +0530

    changes

--- Day 4 LevelCheck/DividedBy3.py
+++ Day 4 LevelCheck/DividedBy3.py
@@ -2,15 +2,15 @@
 Divide again by 3 and print the result. Keep dividing until the number is less than 3. 
 Print the number of times the number was divided."""
 
-num=int(input("Enter the number:"))
-count=0
-no_of_result=1
+num=int(input("Enter the number:"))       #Get a number from the user
+count=0                                   # Initialize the count variable
+no_of_result=1                            # Initialize the number of result variable
 divided_num=int(input("Enter the divided number:"))
-while num>=3:        #loop until the num is less than 
+while num>=3:                 #loop until the num is less than or equal 3
     num//=3
     print("Result",no_of_result,":",num)
-    count+=1
-    no_of_result+=1
+    count+=1                #Increment the count value
+    no_of_result+=1      #Increment the no of result value
 print(count,"times the number was divided")
 
 # OUTPUT:
