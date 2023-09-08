8d644b4edfae7f94844496e46e7e6e76ae01016d
Sivakarthika29 <sivakarthika.sekar29@gmail.com>
2023-09-08 23:20:42 +0530

    commit

--- Day 4 LevelCheck/NoOfTimesEachWord.py
+++ Day 4 LevelCheck/NoOfTimesEachWord.py
@@ -18,5 +18,3 @@ print("Number of times each word is :\n "+ str(all_freq))
 # Enter the sentence:my name is siva my father name is sekar
 # Number of times each word is :
 # {'my': 2, 'name': 2, 'is': 2, 'siva': 1, 'father': 1, 'sekar': 1}
-
-
