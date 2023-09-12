a52a04b5c64da359c8bfe7ae44d89206a25d788a
Sivakarthika29 <sivakarthika.sekar29@gmail.com>
2023-09-12 22:23:06 +0530

    commit

---
+++ Day 14/String2.py
@@ -0,0 +1,20 @@
+########## Program 2
+#Print your name in a pyramid
+#eg RAM
+#R
+#RA
+#RAM
+        
+# myName = #FillinMissingCode
+# for letter in myName:
+#     print(#FillinMissingCode)
+        
+        
+myName = input("Enter the name : ")
+ans = ""
+for letter in myName:
+    ans = ans + letter
+for x in range(len(myName)):
+    for y in range(x + 1):
+        print(ans[y], end=" ")
+    print()
