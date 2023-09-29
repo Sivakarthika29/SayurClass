8c00ff5d66c075767c8a9fc18e45ff2a4fb515b8
Sivakarthika29 <sivakarthika.sekar29@gmail.com>
2023-09-29 17:59:26 +0530

    commit

--- Day 17 ( list, for, if )/Problem2.py
+++ Day 17 ( list, for, if )/Problem2.py
@@ -7,3 +7,14 @@ Grade B , 80 or above
 Fail is < 50
 Print the name of the students who got A in all subjects or atleast one A and the rest B.
 Try to use only one if statement. '''
+
+stud_name = ["Siva" , "Karthika" , "Ranjani" , "Shankar" , "Sekar"]
+cs_mark = [98 , 64 , 96 , 98 , 23]
+math_mark = [90 , 80 , 96 , 48 , 73]
+eng_mark = [86 , 100 , 96 , 88 , 83]
+new_stud_list = []
+
+for i in range(len(stud_name)):
+    if (cs_mark[i] >= 90 or math_mark[i] >= 90 or eng_mark[i] >= 90) and (cs_mark[i] >= 80 and math_mark[i] >= 80 and eng_mark[i] >= 80):
+        new_stud_list.append(stud_name[i])
+print(f"Names of Student scoring A and B grade{new_stud_list}")
