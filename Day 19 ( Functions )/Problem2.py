710d151e22235b71568c1b4dc1611ee8adc3e04d
Sivakarthika29 <sivakarthika.sekar29@gmail.com>
2023-10-04 19:45:29 +0530

    commit

--- Day 19 ( Functions )/Problem2.py
+++ Day 19 ( Functions )/Problem2.py
@@ -6,11 +6,16 @@ def cs_mark(no_of_students , total_mark_of_class):
     avg_mark = total_mark_of_class/no_of_students
     return avg_mark
 
+no_of_exam = 3
+#total_avg = 0
 no_of_students = int(input("Enter no of students : "))
-for i in range(1,4):
+for i in range(no_of_exam):
     total_mark_of_class = int(input("Enter total mark of class in CS subject : "))
     result = cs_mark(no_of_students , total_mark_of_class)
+    #total_avg += result
     print(f"Avg marks of class in CS exam {i} : {int(result)}%")
+#total_avg_of_last_3_exam = total_avg / no_of_exam
+#print(f"Total avg of last 3 exams : {total_avg_of_last_3_exam}")
 
 # OUTPUT:
 
