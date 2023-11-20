ee9a444b7d5fe0ed6590da95697486beda5a3fc4
Sivakarthika29 <sivakarthika.sekar29@gmail.com>
2023-11-20 11:09:04 +0530

    commit

---
+++ Testing/Pattern3.py
@@ -0,0 +1,12 @@
+#
+# #
+# # #
+# # # #
+# # # # #
+
+row = int(input("Enter no of rows : "))
+col = int(input("Enter no of col : "))
+for i in range (row):
+    for j in range(col,0):
+        print("# ",end ="")
+    print()
