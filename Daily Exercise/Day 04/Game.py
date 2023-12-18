4a3b31647185e360418ae7e1c845c54db2269922
Sivakarthika29 <sivakarthika.sekar29@gmail.com>
2023-12-18 11:55:36 +0530

    output added

--- Daily Exercise/Day 04/Game.py
+++ Daily Exercise/Day 04/Game.py
@@ -60,3 +60,273 @@ if player_A_points == winning_points:
     print("***** PLAYER A WON *****")
 else:
     print("***** PLAYER B WON *****")
+
+''' OUTPUT:
+
+Player A turn , enter to roll the dice 
+Row 6 , Col 3
+Player B turn , enter to roll the dice 
+Row 3 , Col 6
+
+* * * * * * 
+* * * * * * 
+* * * * * B 
+* * * * * * 
+* * * * * * 
+* * A * * * 
+
+Player A points -  0
+Player B points -  0
+Player A turn , enter to roll the dice
+Row 4 , Col 4
+Player B turn , enter to roll the dice
+Row 3 , Col 4
+
+* * * * * *
+* * * * * *
+* * * B * B
+* * * A * *
+* * * * * *
+* * A * * *
+
+Player A points -  0
+Player B points -  0
+Player A turn , enter to roll the dice
+Row 3 , Col 6
+Player B turn , enter to roll the dice
+Row 3 , Col 2
+
+* * * * * *
+* * * * * *
+* B * B * A
+* * * A * *
+* * * * * *
+* * A * * *
+
+Player A points -  1
+Player B points -  0
+Player A turn , enter to roll the dice
+Row 1 , Col 4
+Player B turn , enter to roll the dice
+Row 4 , Col 2
+
+* * * A * *
+* * * * * *
+* B * B * A
+* B * A * *
+* * * * * *
+* * A * * *
+
+Player A points -  1
+Player B points -  0
+Player A turn , enter to roll the dice
+Row 3 , Col 1
+Player B turn , enter to roll the dice
+Row 3 , Col 6
+
+* * * A * *
+* * * * * *
+A B * B * B
+* B * A * *
+* * * * * *
+* * A * * *
+
+Player A points -  1
+Player B points -  1
+Player A turn , enter to roll the dice
+Row 5 , Col 6
+Player B turn , enter to roll the dice
+Row 2 , Col 4
+
+* * * A * *
+* * * B * *
+A B * B * B
+* B * A * *
+* * * * * A
+* * A * * *
+
+Player A points -  1
+Player B points -  1
+Player A turn , enter to roll the dice
+Row 3 , Col 2
+Player B turn , enter to roll the dice
+Row 3 , Col 1
+
+* * * A * *
+* * * B * *
+B A * B * B
+* B * A * *
+* * * * * A
+* * A * * *
+
+Player A points -  2
+Player B points -  2
+Player A turn , enter to roll the dice
+Row 2 , Col 1
+Player B turn , enter to roll the dice
+Row 2 , Col 3
+
+* * * A * *
+A * B B * *
+B A * B * B
+* B * A * *
+* * * * * A
+* * A * * *
+
+Player A points -  2
+Player B points -  2
+Player A turn , enter to roll the dice
+Row 3 , Col 5
+Player B turn , enter to roll the dice
+Row 6 , Col 5
+
+* * * A * *
+A * B B * *
+B A * B A B
+* B * A * *
+* * * * * A
+* * A * B *
+
+Player A points -  2
+Player B points -  2
+Player A turn , enter to roll the dice
+Row 6 , Col 5
+Player B turn , enter to roll the dice
+Row 5 , Col 5
+
+* * * A * *
+A * B B * *
+B A * B A B
+* B * A * *
+* * * * B A
+* * A * A *
+
+Player A points -  3
+Player B points -  2
+Player A turn , enter to roll the dice
+Row 1 , Col 1
+Player B turn , enter to roll the dice
+Row 5 , Col 3
+
+A * * A * *
+A * B B * *
+B A * B A B
+* B * A * *
+* * B * B A
+* * A * A *
+
+Player A points -  3
+Player B points -  2
+Player A turn , enter to roll the dice
+Row 2 , Col 3
+Player B turn , enter to roll the dice
+Row 3 , Col 6
+
+A * * A * *
+A * A B * *
+B A * B A B
+* B * A * *
+* * B * B A
+* * A * A *
+
+Player A points -  4
+Player B points -  2
+Player A turn , enter to roll the dice
+Row 3 , Col 3
+Player B turn , enter to roll the dice
+Row 3 , Col 6
+
+A * * A * *
+A * A B * *
+B A A B A B
+* B * A * *
+* * B * B A
+* * A * A *
+
+Player A points -  4
+Player B points -  2
+Player A turn , enter to roll the dice
+Row 2 , Col 1
+Player B turn , enter to roll the dice
+Row 6 , Col 4
+
+A * * A * *
+A * A B * *
+B A A B A B
+* B * A * *
+* * B * B A
+* * A B A *
+
+Player A points -  4
+Player B points -  2
+Player A turn , enter to roll the dice
+Row 4 , Col 4
+Player B turn , enter to roll the dice
+Row 4 , Col 3
+
+A * * A * *
+A * A B * *
+B A A B A B
+* B B A * *
+* * B * B A
+* * A B A *
+
+Player A points -  4
+Player B points -  2
+Player A turn , enter to roll the dice
+Row 1 , Col 2
+Player B turn , enter to roll the dice
+Row 2 , Col 1
+
+A A * A * *
+B * A B * *
+B A A B A B
+* B B A * *
+* * B * B A
+* * A B A *
+
+Player A points -  4
+Player B points -  3
+Player A turn , enter to roll the dice
+Row 5 , Col 4
+Player B turn , enter to roll the dice
+Row 3 , Col 2
+
+A A * A * *
+B * A B * *
+B B A B A B
+* B B A * *
+* * B A B A
+* * A B A *
+
+Player A points -  4
+Player B points -  4
+Player A turn , enter to roll the dice
+Row 1 , Col 6
+Player B turn , enter to roll the dice
+Row 1 , Col 5
+
+A A * A B A
+B * A B * *
+B B A B A B
+* B B A * *
+* * B A B A
+* * A B A *
+
+Player A points -  4
+Player B points -  4
+Player A turn , enter to roll the dice
+Row 1 , Col 5
+Player B turn , enter to roll the dice
+Row 2 , Col 2
+
+A A * A A A
+B B A B * *
+B B A B A B
+* B B A * *
+* * B A B A
+* * A B A *
+
+Player A points -  5
+Player B points -  4
+***** PLAYER A WON ***** '''
